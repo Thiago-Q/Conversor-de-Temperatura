5594b6f4610e34a0640d4dee563cf2c9277ffc74
Thiago-Q <83247602+Thiago-Q@users.noreply.github.com>
2021-05-29 21:36:20 -0400

    Create Conversor.c

---
+++ Conversor.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+  float temperatura_F, conversao_C;
+  printf("Informe a medida em Fahrenheit: ");
+  scanf("%f", &temperatura_F);
+  conversao_C = (temperatura_F - 32) / 1.8;
+  printf("A temperatura em Celsius e: %.2f", conversao_C);
+  return 0;
+}
